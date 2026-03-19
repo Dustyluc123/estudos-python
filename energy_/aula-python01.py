ab59882b8d065570c49241cc28e3f84411338028
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-19 09:27:54 -0300

    lucas

--- energy_/aula-python01.py
+++ energy_/aula-python01.py
@@ -1,15 +1,18 @@
-#AULA 01
-
-#1. inserção dos dados de placa do motor
 
 #Potência Útil (Mecânica)
 #Potência Útil (Mecânica)
 PU = int(input('Insira o valor da potencia util em kw: '))
 n = float(input('Insira o valor do rendimento do motor (de 0 a 1): '))
-
-#2. Calculo de potencia ativa
+FP = float(input('Insira o valor do fator de potência (de 0 a 1): '))
 # Rendimento: 
 #n = PU / p
+# Potência Ativa (em Watss)
 P = PU / n
+# Potência Aparente (em Volt-Ampére)
+S = P / FP
+# Potência Reativa (em Volt-Ampére-Reativo)
+Q = (S**2 - P**2)**(1/2)
 
-print(f'A potencia ativa será de {P:.2f} W')
+print(f'A potencia ativa será de {P:.1f} W')
+print(f'A potencia aparente será de {S:.1f} VA') 
+print(f'A potencia reativa será de {Q:.1f} VAr')
